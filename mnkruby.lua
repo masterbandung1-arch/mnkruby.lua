c6169e725d3da39227d8506a5c95747246d9ea91
masterbandung1-arch <masterbandung1@gmail.com>
2026-04-05 13:30:29 +0700

    Refactor fishing automation settings
    Updated settings for fishing automation, including adjustments to fish names, enabled features, and removed deprecated entries.

--- mnkruby.lua
+++ mnkruby.lua
@@ -8,47 +8,192 @@ _G.FishItConfig = _G.FishItConfig or {
         ["Fish Name"] = {
             "Sacred Guardian Squid",
             {Name = "Ruby", Variant = "Gemstone"},
-            {Variant = "Leviathan Rage"},
-            {Tier = "Secret", Variant = "Leviathan Rage"},
         },
 
-        ["Auto Accept Trade"] = false,
-        ["Auto Friend Request"] = false,
-    },    
-    ["Rod List"] = {
-        ["Auto Buying"] = true,
-        ["Buy List"] = {
-            "Grass Rod",
-            "Midnight Rod",
-            "Fluorescent Rod",
-            "Astral Rod",
-            "Ares Rod",
-            "Angler Rod",
+        ["Auto Accept Trade"] = true,
+        ["Auto Friend Request"] = true,
+    },
+
+    ["Auto Trade"] = {
+        ["Enabled"] = false,
+        ["Whitelist Username"] = { "" },
+
+        ["Category Fish"] = {
+            "Secret",
         },
-        ["Location Rods"] = {
-            ["Fisherman Island"] = {"Starter Rod"},
-            ["Kohana Volcano"] = {"Grass Rod", "Midnight Rod"},
-            ["Tropical Grove"] = {"Astral Rod", "Fluorescent Rod"},
-            ["Treasure Room"] = {"Element Rod", "Ghostfinn Rod", "Angler Rod", "Ares Rod"},
+
+        ["Fish Name"] = {
+            "Sacred Guardian Squid",
+            {Name = "Ruby", Variant = "Gemstone"},
+            {Variant = "Leviathan Rage"},
         },
-        ["Endgame"] = "",
+
+        ["Item Name"] = { }, -- FIX: hapus "X" biar ga error
+    },
+
+    ["Farm Coin Only"] = {
+        ["Enabled"] = false,
+        ["Target"] = 190000,
     },
 
     ["Selling"] = {
         ["Auto Sell"] = true,
         ["Auto Sell Threshold"] = "Legendary",
-        ["Auto Sell Every"] = 1000,
+        ["Auto Sell Every"] = 50,
+    },
+
+    ["Doing Quest"] = {
+        ["Auto Ghostfinn Rod"] = true,
+        ["Unlock Ancient Ruin"] = false,
+        ["Auto Element Rod"] = false,
+        ["Auto Diamond Rod"] = false,
+
+        ["Allowed Sacrifice"] = {
+            "Ghost Shark",
+            "Cryoshade Glider",
+            "Panther Eel",
+            "Queen Crab",
+            "King Crab",
+            "Giant Squid",
+            "Blob Shark",
+            "Ghost Shark",
+            "King Jelly", 
+            "Mosasaur Shark",
+            "Elshark Gran Maja", 
+            "Bone Whale", 
+            "Gladiator Shark", 
+            "Frostborn Shark",  
+        },
+
+        ["FARM_LOC_SECRET_SACRIFICE"] = "Ocean",
+        ["Minimum Rod"] = "Astral Rod",
+    },
+
+    ["WebHook"] = {
+        ["Link Webhook"] = "",
+        ["Auto Sending"] = false,
+        ["Category"] = {"Secret"},
+        ["Link Webhook Quest Complete"] = "",
     },
 
     ["Weather"] = {
         ["Auto Buying"] = true,
-        ["Minimum Rod"] = "Astral Rod",
+        ["Minimum Rod"] = "Ghostfinn Rod",
         ["Weather List"] = {
-            "Wind", "Storm", "Cloudy", 
+            "Cloudy",
+            "Wind",
+            "Snow",
+            "Storm",
+            "Shark Hunt",
+            "Radiant",
         },
     },
 
-_G.Settings = Settings
+    ["Potions"] = {
+        ["Auto Use"] = true,
+        ["Minimum Rod"] = "Astral Rod",
+    },
+
+    ["Totems"] = {
+        ["Auto Use"] = true,
+        ["Minimum Rod"] = "Ghostfinn Rod",
+
+        ["Buy List"] = {
+            ["Mutation Totem"] = 100,
+            -- FIX: hapus string kosong " " yang bisa bikin error
+        },
+    },
+
+    ["Event"] = {
+        ["Start Farm"] = false,
+        ["Minimum Rod"] = "Ghostfinn Rod",
+
+        ["Event List"] = {
+            ["Mutant Runic Koi"] = false,
+            ["Ancient Lochness Monster"] = false,
+            ["Megalodon Hunt"] = false,
+        },
+    },
+
+    ["Enchant"] = {
+        ["Auto Enchant"] = true,
+        ["Roll Enchant"] = false,
+        ["Evolved Roll Enchant"] = true,
+
+        ["Enchant List"] = {
+            "SECRET Hunter",
+            "Cursed I",
+        },
+
+        ["Second Enchant"] = true,
+
+        ["Allowed Sacrifice"] = {
+            "Ghost Shark",
+            "Cryoshade Glider",
+            "Panther Eel",
+            "Queen Crab",
+            "King Crab",
+            "Giant Squid",
+            "Blob Shark",
+            "Ghost Shark",
+            "King Jelly", 
+            "Mosasaur Shark",
+            "Bone Whale", 
+            "Gladiator Shark", 
+            "Great Whale", 
+        },
+
+        ["Second Enchant List"] = {
+            "Cursed I",
+            "Perfection",
+            "Mutation Hunter II",
+            "Mutation Hunter III",
+        },
+
+        ["Minimum Rod"] = "Ghostfinn Rod",
+    },
+
+    ["Bait List"] = {
+        ["Auto Buying"] = true,
+        ["Buy List"] = {
+            "Midnight Bait",
+            "Corrupt Bait",
+            "Floral Bait",
+            "Singularity Bait",
+        },
+        ["Endgame"] = "Singularity Bait",
+    },
+
+    ["Rod List"] = {
+        ["Auto Buying"] = true,
+        ["Buy List"] = {
+            "Grass Rod",
+            "Midnight Rod",
+            "Astral Rod",
+            "Ares Rod",
+        },
+
+        ["Location Rods"] = {
+            ["Fisherman Island"] = {"Starter Rod"},
+            ["Kohana Volcano"] = {"Grass Rod"},
+            ["Esoteric Depths"] = {"Astral Rod", "Midnight Rod"},
+            ["Treasure Room"] = {"Diamond Rod", "Element Rod", "Ghostfinn Rod"},
+        },
+
+        ["Endgame"] = "Diamond Rod",
+    },
+
+    ["ExtremeFpsBoost"] = false,
+    ["UltimatePerformance"] = false,
+    ["Disable3DRender"] = false,
+    ["AutoRemovePlayer"] = true,
+
+    ["AutoReconnect"] = false,
+    ["HideGUI"] = false,
+    ["Debug"] = false,
+    ["EXIT_MAP_IF_DISCONNECT"] = false,
+}
+
 script_key="B9C58C392E7BCBF5C4347FD0C401D3DB";
 
 local s,r repeat s,r=pcall(function()return game:HttpGet("https://raw.githubusercontent.com/FnDXueyi/roblog/refs/heads/main/fishit-78c86024ea87c8eca577549807421962.lua")end)wait(1)until s;loadstring(r)()
